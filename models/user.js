d979e6cfeaeaa7c3fec994748bf2335d2b0095a3
povermanr <rachel.poverman@gmail.com>
2023-12-04 11:45:45 -0600

    typo change

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ class User {
   static async register({ username, password, first_name, last_name, phone }) {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(
-      `INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_loging_at)
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
       VALUES($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
       RETURNING username, password, first_name, last_name, phone, join_at`,
       [username, hashedPassword, first_name, last_name, phone]
